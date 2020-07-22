a34dd3a2b02243daf4c2febb863c22f3154d4568
bhavikbhoir <bhvkbhoir95@gmail.com>
2020-07-22 00:31:55 -0400

    Counter updated

--- src/components/decimalcounter.js
+++ src/components/decimalcounter.js
@@ -24,18 +24,14 @@ export default class Decimalcounter extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-  handleload(){
-    this.setState({count: parseFloat(this.state.seconds).toFixed(2)});
-      localStorage.setItem(count_LS, parseFloat(this.state.seconds).toFixed(2));
-  }
-  resetcount =(e) => {
+  resetcount = (e) => {
     localStorage.clear();
     this.setState({seconds: 1000.00});
   }
   render() {
       return (
         <div>
-          <button id="counter-btn" onLoad={this.handleload}><p>{(this.state.seconds).toFixed(2)}</p></button>
+          <button id="counter-btn"><p>{(this.state.seconds).toFixed(2)}</p></button>
           <div><button id="reset-btn" onClick={this.resetcount}>Reset</button></div>
         </div>
       )
